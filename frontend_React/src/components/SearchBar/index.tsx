734fe7547d0a06e10bdadeb13c10321a01a8dea0
ThiagoDRangel <info.tec.campos@gmail.com>
2023-09-22 10:56:40 -0300

    feat: Catalog nextPage

--- frontend_React/src/components/SearchBar/index.tsx
+++ frontend_React/src/components/SearchBar/index.tsx
@@ -1,12 +1,45 @@
+import { useState } from 'react';
 import './styles.css';
 
-function SearchBar() {
+type SearchFunction = (searchQuery: string) => void;
+
+type Props = {
+  onSearch: SearchFunction;
+}
+
+function SearchBar({ onSearch }: Props) {
+
+  const [text, setText] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  function handleResetClick() {
+    setText("");
+    onSearch(text);
+  }
 
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
-      <button type="reset">🗙</button>
+      <input
+        type="text"
+        placeholder="Nome do produto"
+        onChange={handleChange}
+        value={text}
+      />
+      <button
+        onClick={handleResetClick}
+
+      >
+        🗙
+      </button>
     </form>
   );
 }
--- frontend_React/src/routes/ClientHome/Catalog/index.tsx
+++ frontend_React/src/routes/ClientHome/Catalog/index.tsx
@@ -6,22 +6,43 @@ import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import './styles.css';
 
+type QueryParams = {
+    page: number;
+    name: string;
+}
+
 export default function Catalog() {
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
+    const [queryParams, setQueryParams] = useState<QueryParams>({
+        page: 0,
+        name: ""
+    });
+    const [isLastPage, setIsLastPage] = useState(false);
 
     useEffect(() => {
 
-        productService.findPageRequest(0, "ma")
+        productService.findPageRequest(queryParams.page, queryParams.name)
             .then(response => {
-                setProducts(response.data.content);
+                const nextPage = response.data.content
+                setProducts(products.concat(nextPage));
+                setIsLastPage(response.data.last);
             });
-    }, []);
+    }, [queryParams]);
+
+    function handleSearch(searchText: string) {
+        setProducts([]);
+        setQueryParams({...queryParams, page: 0, name: searchText});
+    }
+
+    function handleNextPageClick() {
+        setQueryParams({ ...queryParams, page: queryParams.page + 1 });
+    }
 
     return (
         <main>
             <section id="catalog-section" className="dsc-container">
-                <SearchBar />
+                <SearchBar onSearch={handleSearch} />
 
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                     {
@@ -31,7 +52,12 @@ export default function Catalog() {
                     }
                 </div>
 
-                <ButtonNextPage />
+                { 
+                    !isLastPage &&
+                    <div onClick={handleNextPageClick}>
+                        <ButtonNextPage />
+                    </div>
+                }
             </section>
         </main>
     );
