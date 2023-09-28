1af3f00427f1eb3a4d5e6bc9d4f809ddcd51191f
ThiagoDRangel <info.tec.campos@gmail.com>
2023-09-28 17:53:43 -0300

    fix: deploy ready

--- frontend_React/src/main.tsx
+++ frontend_React/src/main.tsx
@@ -1,6 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+window.React = React
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
