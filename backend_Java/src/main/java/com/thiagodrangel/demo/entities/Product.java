de32db3629b27dff88098b14bc75da66f98a0d51
ThiagoDRangel <info.tec.campos@gmail.com>
2024-05-20 12:37:47 -0300

    feat: Implements List Order in Product and List Product in Order

--- backend_Java/src/main/java/com/thiagodrangel/demo/entities/Product.java
+++ backend_Java/src/main/java/com/thiagodrangel/demo/entities/Product.java
@@ -3,6 +3,7 @@ package com.thiagodrangel.demo.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -78,5 +79,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order>  getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
